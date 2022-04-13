6737cb02fed817456b809791dc911322e8726931
Andy O'Neill <Andrew.X.Oneill.-ND@disney.com>
2022-04-13 18:55:25 -0400

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `OBS Overlay`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-emotion"]
+};
+
+export default config;
